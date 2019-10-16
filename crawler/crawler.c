516a7304aa26536bfb5a4d86d68174e0a4eb338a
Jade0053 <jadechen199901@outlook.com>
2019-10-16 18:11:22 -0400

    step3 finished

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -10,26 +10,47 @@
  */
 
 #include <stdio.h>
+#include <webpage.h>
+#include <queue.h>
+#include <stdlib.h>
+
+void printy(void* element){
+	webpage_t* wp = (webpage_t*)element;
+	printf("%s\n",webpage_getURL(wp));
+	
+	webpage_delete((void*)wp);
+}
 
 int main(void) {
 
-	char* url = "https://thayer.github.io/eng50";
-	char* html;
-	webpage_t* webby = webpage_new(url, 0, html);
+	char* url = "https://thayer.github.io/engs50/";
+	char* html = NULL;
+	char* result;
+	int pos = 0;
+	queue_t* qp = qopen(); 
+	
+	webpage_t* webby = webpage_new(url, 0, NULL);
 
-	html = webpage_getHTML(webby);
-
-	if (html==NULL){
+	if (webpage_fetch(webby)){
+		html = webpage_getHTML(webby);	
+	}
+	else if (html == NULL)
 		exit(EXIT_FAILURE);
+
+	while ((pos = webpage_getNextURL(webby, pos, &result)) > 0) {
+		printf("Found url: %s  ", result);
+		if (IsInternalURL(result)){
+			webpage_t* inter_web = webpage_new(result, 1, NULL);
+			qput(qp,(void*)inter_web);
+			printf("Internal URL.\n");
+		}
+		else
+			printf("External URL.\n");
+		free(result);
 	}
 
-	
-	// log one word (1-9 chars) about a given url
-	inline static void logr(const char *word, const int depth, const char *url)
-	{
-  printf("%2d %*s%9s: %s\n", depth, depth, "", word, url);
-	}
-	
+	qapply(qp,printy);
+	qclose(qp);
 
 	webpage_delete((void*) webby);
 	exit(EXIT_SUCCESS);
